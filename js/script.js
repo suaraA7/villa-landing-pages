12095f5a81c33ee79800954d27acaeb22ca02410
suara <suudahpengalaman123@gmail.com>
2025-04-28 15:25:21 +0800

    menambahkan gambar

--- js/script.js
+++ js/script.js
@@ -2,12 +2,30 @@
 document.getElementById('navbar-toggler').addEventListener('click', function () {
   var imgElement = this.querySelector('img');
 
-  if (imgElement.src.includes('handburger-icon.png')) {
-    imgElement.src = './images/handburger-icon-close.png';
+  if (imgElement.src.includes('hamburger-menu.png')) {
+    imgElement.src = './images/hamburger-menu-close.png';
     document.getElementById("navbarNav").classList.remove('d-none');
   } else {
-    imgElement.src = './images/handburger-icon.png';
+    imgElement.src = './images/hamburger-menu.png';
     document.getElementById("navbarNav").classList.add('d-none ');
   }
   this.style.outline = 'none';
 });
+
+//FAQ Section JS
+function toggleFaq(item) {
+  const answer = item.querySelector('.faq-answer');
+  const icon = item.querySelector('.faq-icon');
+
+  const isVisible = answer.style.display === 'block';
+
+  // Tutup semua yang terbuka
+  document.querySelectorAll('.faq-answer').forEach(el => el.style.display = 'none');
+  document.querySelectorAll('.faq-icon').forEach(el => el.classList.remove('rotate-icon'));
+
+  // Toggle item sekarang
+  if (!isVisible) {
+      answer.style.display = 'block';
+      icon.classList.add('rotate-icon');
+  }
+}
